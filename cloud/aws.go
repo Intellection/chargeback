e0593e979b4c88bf71fc3bf81cffeb3c8d6d6653
King'ori Maina <j@kingori.co>
2018-05-10 12:34:23 +0300

    Add method to fetch instance current spot price

--- cloud/aws.go
+++ cloud/aws.go
@@ -2,6 +2,7 @@ package cloud
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -17,11 +18,12 @@ type AWSEBSVolume struct {
 
 // AWSInstance is an AWS EBS instance
 type AWSInstance struct {
-	ID         string
-	EBSVolumes []AWSEBSVolume
-	Lifecycle  string
-	Region     string
-	Type       string
+	ID               string
+	AvailabilityZone string
+	EBSVolumes       []AWSEBSVolume
+	Lifecycle        string
+	Region           string
+	Type             string
 }
 
 // GetAWSInstanceInfo gets information on an AWS instance
@@ -61,11 +63,12 @@ func GetAWSInstanceInfo(id string, region string) (AWSInstance, error) {
 	}
 
 	instance = AWSInstance{
-		ID:         *result.Reservations[0].Instances[0].InstanceId,
-		EBSVolumes: ebsVolumes,
-		Lifecycle:  lifecycle,
-		Region:     region,
-		Type:       *result.Reservations[0].Instances[0].InstanceType,
+		ID:               *result.Reservations[0].Instances[0].InstanceId,
+		AvailabilityZone: *result.Reservations[0].Instances[0].Placement.AvailabilityZone,
+		EBSVolumes:       ebsVolumes,
+		Lifecycle:        lifecycle,
+		Region:           region,
+		Type:             *result.Reservations[0].Instances[0].InstanceType,
 	}
 
 	return instance, nil
@@ -94,6 +97,61 @@ func GetAWSInstanceOnDemandHourlyPrice(instance *AWSInstance) (float64, bool, er
 
 			break
 		}
+	}
+
+	return pricePerHour, priceFound, nil
+}
+
+// GetAWSInstanceSpotHourlyPrice fetches current AWS spot price of an instance.
+func GetAWSInstanceSpotHourlyPrice(instance *AWSInstance) (float64, bool, error) {
+	var pricePerHour float64
+	var priceFound bool
+
+	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String(instance.Region)}))
+	svc := ec2.New(sess)
+	input := &ec2.DescribeSpotPriceHistoryInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("availability-zone"),
+				Values: []*string{
+					aws.String(instance.AvailabilityZone),
+				},
+			},
+			{
+				Name: aws.String("instance-type"),
+				Values: []*string{
+					aws.String(instance.Type),
+				},
+			},
+			{
+				Name: aws.String("product-description"),
+				Values: []*string{
+					aws.String("Linux/UNIX (Amazon VPC)"),
+				},
+			},
+		},
+	}
+
+	result, err := svc.DescribeSpotPriceHistory(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return pricePerHour, priceFound, err
+	}
+
+	if len(result.SpotPriceHistory) > 0 {
+		pricePerHour, err = strconv.ParseFloat(*result.SpotPriceHistory[0].SpotPrice, 64)
+		if err != nil {
+			return pricePerHour, priceFound, err
+		}
+
+		priceFound = true
 	}
 
 	return pricePerHour, priceFound, nil
